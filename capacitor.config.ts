88230d36c53a7d89d7280954f474c99e6beb9b21
miaoluyu <miaoluyu@cyjy.com>
2026-05-15 17:36:34 +0800

    bug3

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.question',
+  appName: 'QuestionApp',
+  webDir: 'dist'
+};
+
+export default config;
